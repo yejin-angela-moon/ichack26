df55ac47f8fdbc6b3113411b988aa019b57030f7
sauceboss44 <alexrudakov5@gmail.com>
2026-01-31 16:55:47 +0000

    Add test-slack script

--- src/services/SlackService.js
+++ src/services/SlackService.js
@@ -3,20 +3,20 @@ const { IncomingWebhook } = require('@slack/webhook');
 const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
 
 async function sendSlackNotification(message) {
-    await webhook.send({
-      text: `New notification`,
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: message,
-          },
+  await webhook.send({
+    text: `New notification`,
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: message,
         },
-      ],
-    });
+      },
+    ],
+  });
 }
 
 module.exports = {
-    sendSlackNotification,
+  sendSlackNotification,
 };
